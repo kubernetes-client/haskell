7d547f8332ae423f7eeac41a4c128b748f363f7a
dinkar <dinkar.ganti@gmail.com>
2018-05-06 14:12:21 -0400

    adding examples

---
+++ kubernetes-exec-helper/examples/Exec.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{- | 
+  A simple pod example based on the example [here](https://github.com/kubernetes-client/python/blob/master/examples/exec.py)
+. 
+-}
+module Main where
+
+import System.IO
+import Kubernetes.WSStream
+import Data.Yaml (decodeFile, decodeEither, decodeFileEither, ParseException)
+import Data.Maybe (fromJust)
+import Kubernetes.KubeConfig(Config, getContext, getCluster)
+import Options.Applicative 
+import Data.Semigroup ((<>))
+
+data Settings = Settings {
+  settingsFile :: FilePath 
+}
+
+parseSettings :: Parser Settings 
+parseSettings = Settings
+  <$> strOption 
+        (long "yaml"
+          <> metavar "yaml"
+          <> help "Configuration file for k8s."
+        )
+
+parse :: Settings -> IO (Either ParseException Config)
+parse (Settings fileName) = decodeFileEither fileName
+
+exec :: Settings -> IO ()
+exec settings  = do 
+  config <- parse settings
+  context <- return $ getContext <$> config
+  cluster <- return $ getCluster <$> config
+  putStrLn $ "read context" <> (show config)
+  return ()
+   
+main = do 
+  exec =<< execParser opts 
+  where 
+    opts = info parseSettings (fullDesc)
