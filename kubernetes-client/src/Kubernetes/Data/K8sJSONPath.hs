c0e41edcccc644f8e17d5ad6b76d46e871aac3f1
Tom McLaughlin <tom@codedown.io>
2023-06-26 00:23:43 -0700

    Version with megaparsec and attoparsec

--- kubernetes-client/src/Kubernetes/Data/K8sJSONPath.hs
+++ kubernetes-client/src/Kubernetes/Data/K8sJSONPath.hs
@@ -5,29 +5,53 @@ module Kubernetes.Data.K8sJSONPath where
 import Control.Applicative  ((<|>))
 import Data.Aeson
 import Data.Aeson.Text
-import Data.Attoparsec.Text ( many1, char, takeWhile1, Parser )
+import Data.Bifunctor
 import Data.JSONPath
 import Data.Monoid     ((<>))
 import Data.Text       as Text
 import Data.Text.Lazy       (toStrict)
+
+#if MIN_VERSION_jsonpath(0,3,0)
+import Data.Void (Void)
+import Text.Megaparsec ( Parsec, eof, runParser, some, takeWhile1P )
+import Text.Megaparsec.Char ( char )
+type Parser a = Parsec Void Text a
+#else
+import Data.Attoparsec.Text ( many1, char, takeWhile1, Parser )
+#endif
 
 
 data K8sPathElement = PlainText Text
                     | JSONPath [JSONPathElement]
   deriving  (Show, Eq)
 
+parseK8sJSONPath :: Text -> Either String [K8sPathElement]
+#if MIN_VERSION_jsonpath(0,3,0)
+parseK8sJSONPath = first show . runParser (k8sJSONPath <* eof) "nothing"
+#else
+parseK8sJSONPath = parseOnly (k8sJSONPath <* endOfInput)
+#endif
+
 k8sJSONPath :: Parser [K8sPathElement]
+#if MIN_VERSION_jsonpath(0,3,0)
+k8sJSONPath = some pathElementParser
+#else
 k8sJSONPath = many1 pathElementParser
+#endif
 
 pathElementParser :: Parser K8sPathElement
 pathElementParser = jsonpathParser <|> plainTextParser
 
 plainTextParser :: Parser K8sPathElement
+#if MIN_VERSION_jsonpath(0,3,0)
+plainTextParser = PlainText <$> takeWhile1P (Just "non_open_brace") (/= '{')
+#else
 plainTextParser = PlainText <$> takeWhile1 (/= '{')
+#endif
 
 jsonpathParser :: Parser K8sPathElement
 #if MIN_VERSION_jsonpath(0,3,0)
-jsonpathParser = JSONPath <$> (char '{' *> jsonPath undefined <* char '}')
+jsonpathParser = JSONPath <$> (char '{' *> jsonPath (char '}') <* char '}')
 #else
 jsonpathParser = JSONPath <$> (char '{' *> jsonPath <* char '}')
 #endif
--- kubernetes-client/test/Kubernetes/Data/K8sJSONPathSpec.hs
+++ kubernetes-client/test/Kubernetes/Data/K8sJSONPathSpec.hs
@@ -1,13 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
+
 module Kubernetes.Data.K8sJSONPathSpec where
 
 import Test.Hspec
-import Test.Hspec.Attoparsec
 
 import Kubernetes.Data.K8sJSONPath
 import Data.Text
 import Data.JSONPath
 import Data.Aeson
+
+#if MIN_VERSION_jsonpath(0,3,0)
+import Data.Void (Void)
+import Test.Hspec.Megaparsec
+import Text.Megaparsec (runParser)
+import Text.Megaparsec.Error (ParseErrorBundle)
+
+(~>) :: Text -> Parser [K8sPathElement] -> Either (ParseErrorBundle Text Void) [K8sPathElement]
+(~>) text parser = runParser parser "nothing" text
+#else
+import Test.Hspec.Attoparsec
+#endif
+
 
 spec :: Spec
 spec = do
@@ -30,4 +44,3 @@ spec = do
         let path = [PlainText "kind is ", JSONPath [KeyChild "kind"]]
             val = (object ["kind" .= ("Pod" :: Text)])
         runJSONPath path val `shouldBe` Right "kind is Pod"
-
